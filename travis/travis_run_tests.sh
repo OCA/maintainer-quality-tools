4cdf4a815fb380dd08efbc465fc616074a31b5dc
Alexandre Fayolle <afayolle.ml@free.fr>
2014-07-01 16:41:53 +0200

    improve test script, fix test script name in sample file

--- travis/travis_run_tests.sh
+++ travis/travis_run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#
+# USAGE
+# travis_run_tests.sh VERSION DB_NAME [dependency_repo_1...]
+
 version=$1
 database=$2
 shift 2
@@ -40,12 +44,12 @@ psql -c 'create database openerp_test with owner openerp;' -U postgres
 coverage run /usr/bin/openerp-server --db_user=openerp --db_password=admin --db_host=localhost -d ${database} ${options} \
     --stop-after-init \
     --addons-path=${addons_path} \
-    -i tested_addons 
+    -i tested_addons | tee stdout.log
 
 if $(grep -v mail stdout.log | grep -q ERROR)
 then
-exit 1
+    exit 1
 else
-exit 0
+    exit 0
 fi
 
