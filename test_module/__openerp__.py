ae61b4d0060595560245d69186d841239a5311e4
Daniel Reis <dgreis@sapo.pt>
2014-07-14 11:34:48 +0100

    Fix test_module PEP8 (duh!)

--- test_module/__openerp__.py
+++ test_module/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-'name' : 'Empty module for tests',
-'version' : '1.0',
-'depends' : ['base'],
+    'name': 'Empty module for tests',
+    'version': '1.0',
+    'depends': ['base'],
 }
