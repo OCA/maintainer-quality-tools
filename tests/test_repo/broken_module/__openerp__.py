decb6d649c39621b060511837ecd56a92426e068
Daniel Reis <dreis.pt@gmail.com>
2014-08-06 20:52:53 +0100

    Add broken module for tests

---
+++ tests/test_repo/broken_module/__openerp__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Broken module for tests',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['broken_view.xml'],
+    'installable': False,  # wait for the self test machinery
+}
