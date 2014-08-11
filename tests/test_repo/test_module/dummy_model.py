78026c8eab67624f56425612765fc7b0b8e8bd48
Daniel Reis <dgreis@sapo.pt>
2014-08-11 15:03:14 +0100

    Generate unicode error

--- tests/test_repo/test_module/dummy_model.py
+++ tests/test_repo/test_module/dummy_model.py
@@ -9,3 +9,9 @@ class DummyModel(orm.Model):
     _columns = {
         'name': fields.char('Dummy', size=100),
     }
+
+
+# printout non-ASCII text to check unicode issues
+# (from Monty Python's "Italian Lesson"...)
+print("Eeeeeee! Milano è tanto meglio di Napoli. "
+      "Milano è la citta la più bella di tutti ... nel mondo...")
