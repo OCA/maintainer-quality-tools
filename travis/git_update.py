0b04413fb1799469ed85d30fbfa00a5edcda989e
Moisés López <moylop260@gmail.com>
2014-11-30 20:12:47 -0600

    [REF] git_update: Add owner and repo data remote

--- travis/git_update.py
+++ travis/git_update.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import re
 import subprocess
 
 
@@ -46,13 +47,30 @@ def git_reset_remote(repo, remote_name, path):
     return res
 
 
+def get_repo_data(repo):
+    regex = "(?P<host>(git@|https://)([\w\.@]+)(/|:))(?P<owner>[~\w,\-,\_]+)/(?P<repo>[\w,\-,\_]+)(.git){0,1}((/){0,1})"
+    match_object = re.search(regex, repo)
+    repo_data = {}
+    if match_object:
+        repo_data['host'] = match_object.group("host")
+        repo_data['owner'] = match_object.group("owner")
+        repo_data['repo'] = match_object.group("repo")
+    return repo_data
+
+
 def git_clone_update(repo, branch, path=None):
+    repo_data = get_repo_data(repo)
+    remote_name = 'origin'
+    if repo_data.get('owner', False):
+        remote_name = repo_data['owner']
     if not path:
         path = os.getcwd()
-    remote_name = 'origin'
+        if repo_data.get('repo', False):
+            path = os.path.join(path, repo_data['repo'])
     git_reset_remote(repo, remote_name, path)
     run_git_cmd(["fetch", remote_name], path)
-    run_git_cmd(["checkout", "-f", branch], path=path)
+    run_git_cmd(["checkout", "-f",
+        remote_name + '/' + branch], path=path)
     run_git_cmd(["reset", "--hard", branch], path=path)
 
 
@@ -64,10 +82,10 @@ def main():
     parser.add_argument("repo_url",
                        help="url of repository"
                              " source of clone.")
+    #  TODO: Set path optional (positional argument)
     parser.add_argument("path",
                         help="Local path to save"
-                             " tree files cloned.",
-                        default='.')
+                             " tree files cloned.",)
     parser.add_argument("-b", dest="branch",
                         help="The name of the branch"
                              "source of clone.")
