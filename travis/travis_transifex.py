e13e3c1c3b5e7e6e9994434cc017797ab5e8e916
Agathe Mollé <agathe.molle@savoirfairelinux.com>
2015-05-07 15:24:36 -0400

    Use trans_export in openerp.tools instead of subprocess call.
    Add Odoo connection classes which describe how to connect to Odoo to export PO files.

--- travis/travis_transifex.py
+++ travis/travis_transifex.py
@@ -5,10 +5,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import os
 import sys
+from odoo_connection import context_mapping
 from test_server import setup_server, get_addons_path, \
     get_server_path, get_addons_to_check
-import subprocess
-from travis_helpers import fail_msg, yellow, yellow_light
+from travis_helpers import yellow, yellow_light
 from txclib import utils, commands
 
 
@@ -48,7 +48,6 @@ def main(argv=None):
     odoo_full = os.environ.get("ODOO_REPO", "odoo/odoo")
     server_path = get_server_path(odoo_full, odoo_version, travis_home)
     addons_path = get_addons_path(travis_home, travis_build_dir, server_path)
-
     addons_list = get_addons_to_check(travis_build_dir, odoo_include,
                                       odoo_exclude)
     addons = ','.join(addons_list)
@@ -79,42 +78,29 @@ def main(argv=None):
     repo_name = "%s-%s" % (travis_repo_slug.split("/")[1],
                            odoo_version.replace('.', '-'))
 
-    # Export translation files and push them to Transifex
-    cmd_export = ["%s/openerp-server" % server_path,
-                  "-d", database,
-                  "--stop-after-init",
-                  "--addons-path", addons_path,
-                  ]
+    connection_context = context_mapping[odoo_version]
+    with connection_context(server_path, addons_path, database) as odoo_context:
+        for module in addons_list:
+            print()
+            print(yellow("Downloading PO file for %s" % module))
+            source_filename = os.path.join(travis_build_dir, module, 'i18n',
+                                           module + ".pot")
+            # Create i18n/ directory if doesn't exist
+            if not os.path.exists(os.path.dirname(source_filename)):
+                os.makedirs(os.path.dirname(source_filename))
+            with open(source_filename, 'w') as f:
+                f.write(odoo_context.get_pot_contents(module))
 
-    for module in addons_list:
-        print()
-        print(yellow("Downloading PO file for %s" % module))
-        module_dir = travis_build_dir + "/" + module
-        source_path = module_dir + "/i18n/" + module + ".po"
-        cmd_export += ["--i18n-export", source_path,
-                       "--modules", module]
-        print(' '.join(cmd_export))
-        pipe = subprocess.Popen(cmd_export,
-                                stderr=subprocess.STDOUT,
-                                stdout=subprocess.PIPE)
-        with open('stdout.log', 'w') as stdout:
-            for line in pipe.stdout:
-                stdout.write(line)
-                print(line.strip())
-        returncode = pipe.wait()
-        if returncode != 0:
-            print(fail_msg, "Command exited with code %s" % returncode)
-
-        print()
-        print(yellow("Linking PO file and Transifex resource"))
-        set_args = ['-t', 'PO',
-                    '--auto-local',
-                    '-r', '%s.%s' % (repo_name, module),
-                    '%s/i18n/<lang>.po' % module,
-                    '--source-lang', 'en',
-                    '--source-file', source_path,
-                    '--execute']
-        commands.cmd_set(set_args, path_to_tx)
+            print()
+            print(yellow("Linking PO file and Transifex resource"))
+            set_args = ['-t', 'PO',
+                        '--auto-local',
+                        '-r', '%s.%s' % (repo_name, module),
+                        '%s/i18n/<lang>.po' % module,
+                        '--source-lang', 'en',
+                        '--source-file', source_filename,
+                        '--execute']
+            commands.cmd_set(set_args, path_to_tx)
 
     print()
     print(yellow('Pushing translation files to Transifex'))
