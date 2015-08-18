1e69c8c420a708ab970421cd7a8c2f605e9dacb1
Moises Lopez <moylop260@vauxoo.com>
2015-08-18 04:09:15 +0000

    [REF] test_pylint: Add feature to validate expected errors with a counter of pylint fails vs a environ variable

--- tests/test_repo/broken_module/model.py
+++ tests/test_repo/broken_module/model.py
@@ -1,8 +1,72 @@
+
 from openerp.osv import orm, fields
 
+import os
+import os as os2  # W0404 - duplicated import
 
-class TestModel(orm.Model):
+import __openerp__  # W0403 - relative import
+
+# w0402 - deprecated module
+import pdb  # pylint: disable=W0403
+import pudb  # pylint: disable=W0403
+import ipdb  # pylint: disable=W0403
+
+
+class test_model(orm.Model):
     _name = "test.model"
     _columns = {
         'name': fields.char('Title', 100),
     }
+
+    def method_test(self, arg1, arg2):
+        return None
+
+    def method_e1124(self):
+        value = self.method_test(
+            'arg_param1', arg1='arg_param1')
+        return value
+
+    def method_e1306(self):
+        return "%s %s" % ('value1')
+
+    def method_e1601(self):
+        print "Hello world!"
+
+    def method_w0101(self):
+        return True
+        return False
+
+    def method_w0102(self, arg1, arg2=[]):
+        # avoid imported but unused
+        all_imports = (
+            os, os2, __openerp__, pdb, pudb, ipdb)
+        return all_imports
+
+    def method_w0104_w0105(self):
+        2*6*0
+        "str any effect"
+
+    def method_w0109(self):
+        my_duplicated_key_dict = {
+            'key1': 'value1',
+            'key2': 'value2',
+            'key1': 'value3',
+        }
+        return my_duplicated_key_dict
+
+    def method_w1401(self):
+        my_regex_str_bad = '\d'
+        my_regex_str_good = r'\d'
+        return my_regex_str_bad, my_regex_str_good
+
+
+if __name__ == '__main__':
+
+    def method_w1111():
+        return None
+
+    VAR1 = method_w1111()
+
+    class E0101(object):
+        def __init__(self):
+            return 'E0101'
