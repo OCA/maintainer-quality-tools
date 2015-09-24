ef7d9b2caac7a862643d01537c27d56febddcdc3
Daniel Reis <dgreis@sapo.pt>
2015-09-24 09:49:46 +0100

    Merge pull request #253 from vauxoo-dev/oca-enable-translation-field
    [IMP] pylint: Enable check to verify fields with _('string')

---
+++ tests/test_repo/broken_lint/case_fields.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from openerp import fields, models
+from openerp.tools.translate import _
+
+
+class MyExampleField(models.Model):
+    _name = "my.example.field"
+    _columns = {
+        'name': fields.char(_('Title'), 100),
+        'description': fields.char(string=_('Description'), size=100),
+    }
+
+
+class MyExampleField2(models.Model):
+    _name = "my.example.field2"
+
+    name = fields.Char(_('Title'), 100)
+    description = fields.Char(name=_('Description'), size=100)
+
+    def my_method1(self, var):
+        pass
+
+    def my_method2(self):
+        return self.my_method1(_('Hello world'))
