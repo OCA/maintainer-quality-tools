5f4fc2e7082087b445aad70a1a954328973047dc
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-04 16:06:40 +0200

    Add uninstallable broken module in test repo.
    Let's see what it does on tests.

---
+++ tests/test_repo/broken_uninstallable/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+from . import model
+from . import interpreter_wox
+# execution permission and interpreter done
---
+++ tests/test_repo/broken_uninstallable/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Broken uninstallable module for tests',
+    # missing license
+    'author': 'Many People',  # Missing oca author
+    'description': 'Should be a README.rst file',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['model_view.xml'],
+    'test': ['test.yml'],
+    'installable': False,
+    'name': 'Duplicated value',
+    'active': True,  # Deprecated active key
+}
