d6dacdcd698f92b200b53ccdd55351f853b0d649
Daniel Reis <dgreis@sapo.pt>
2015-10-18 14:06:56 +0000

    Dummy module for dependency installation tests

---
+++ dependency_module/README.rst
@@ -0,0 +1,7 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Test module
+===========
+
+This module was written to check dependency repos installation.
---
+++ dependency_module/__openerp__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Module for Dependency Tests',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA)',
+    'version': '8.0.0.0.0',
+    'depends': [
+        'base',
+    ],
+}
