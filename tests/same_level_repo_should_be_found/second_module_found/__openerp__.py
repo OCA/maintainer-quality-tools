92e449752c5fabfe68660eeb0467fdf350942d36
David Arnold <dar@devco.co>
2017-01-13 21:18:04 -0500

    [ADD] tests for recursive detection

---
+++ tests/same_level_repo_should_be_found/second_module_found/__openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Second empty module for tests',
+    'version': '8.0.1.0.0',
+    'author': 'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'test_module',
+        ],
+}
---
+++ tests/same_level_repo_should_be_found/test_module_found/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Empty module for tests',
+    'license': 'AGPL-3',
+    'author': 'Odoo Community Association (OCA)',
+    'version': '1.0',
+    'depends': [
+        'base',
+        'dependency_module',  # test dependency repo installation
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
