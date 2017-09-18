4bd508f94e1f6c3d8a1dff8a9795ce044bdbec74
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2017-09-18 18:56:43 -0500

    [REF] broken_deprecated: Use correct version format
    After merge https://github.com/OCA/pylint-odoo/pull/155 we had this unexpected wrong format version

--- tests/test_repo/broken_deprecated/__openerp__.py
+++ tests/test_repo/broken_deprecated/__openerp__.py
@@ -3,7 +3,7 @@
     'name': 'Broken deprecated module for tests MQT',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA)',
-    'version': '8.0.0.1.0.0',
+    'version': '8.0.1.0.0',
     'depends': [
         'base',
     ],
