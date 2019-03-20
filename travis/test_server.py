0899aa7bb0586996da2e20b72557f8602fe9d418
luisiniguezh <luis.iniguezh@gmail.com>
2019-03-20 12:12:43 -0600

    removing init for already initialized db

--- travis/test_server.py
+++ travis/test_server.py
@@ -426,7 +426,8 @@ def main(argv=None):
                                 if item not in rm_items] + \
                     ['--pidfile=/tmp/odoo.pid']
             else:
-                command[-1] = to_test
+                if not "background_no_stop" in argv:
+                    command[-1] = to_test
                 # Run test command; unbuffer keeps output colors
                 command_call = (["unbuffer"] if unbuffer else []) + command
             print(" ".join(cmd_strip_secret(command_call)))
